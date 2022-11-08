538518b5f327c6d204b147dca2f5db0d5584351b
YS739 <suk921@gmail.com>
2022-11-08 10:38:42 +0900

    sp python basic class

---
+++ hello.py
@@ -0,0 +1,34 @@
+# print('hello sparta')
+
+# a = 3
+# b = 2
+#
+# print(a%b)
+
+# a = 'yunny'
+#
+# print(a)
+#
+# a = (3 == 2)
+#
+# print(a)
+#
+# first_name = 'yunny'
+# last_name = ' jeong'
+#
+# print(first_name+last_name)
+
+# a = '2'
+# b = str(2)
+#
+# print(a+b)
+
+# text = 'fafasfsdf'
+#
+# result = text[:]
+# print(result)
+
+# 퀴즈(내가 푼 것)
+text = 'sparta'
+result = text[:3]
+print(result)
